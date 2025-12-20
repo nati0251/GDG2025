493b3bde21adc710f7e556c42c7a59f957826fd6
nati0251 <natnaelbetru123@gmail.com>
2025-12-21 01:05:50 +0300

    week 3 completed

--- nano.py
+++ nano.py
@@ -1,5 +1,13 @@
-x=int(input("number of items you want to buy "))
-if x>3:
-    print("discount applied")
-else:
-    print("no discount")
+import json
+from abc import abstractmethod,ABC
+class Employee(ABC):
+    @abstractmethod
+    def calculate_salary():
+        pass
+
+class Fulltime(Employee):
+    def calculate_salary(self):
+        return 15000
+
+n=Fulltime.calculate_salary()
+print(n)
--- week_2.py
+++ week_2.py
@@ -1,287 +1,135 @@
-# question 1
-x=int(input("number of items you want to buy "))
-if x>3:
-    print("discount applied")
-else:
-    print("no discount")
-
-# question 2
-def get_expensive_items(prices):
-    result = []
-    i = 0
+# # question 1
+# class person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
     
-    while i < len(prices):
-        if prices[i] > 100:
-            result.append(prices[i])
-        i += 1
+#     def introduce(self):
+#         return f"i am {self.name},{self.age} years old"
+# p1=person("nati",22)
+# print(p1.introduce())
+
+# # question 2
+# class Dog:
+#     def __init__(self,sound):
+#         self.sound=sound
     
-    return result
+#     def bark(self):
+#         return f"{self.sound}"
+# dog=Dog("woof")
+# print(dog.bark())
 
-prices = [120, 45, 300, 85, 150]
-result = get_expensive_items(prices)
-print(result)
+#question 3
+# class car:
+#     def __init__(self,make):
+#         self.make=make
+    
+#     def get_make(self):
+#         return f"{self.make}"
+# cars=car("toyota")
+# print(cars.get_make())
 
-# question 3
-def log_user_activity():
-    with open("log.txt", "a") as file:
-        file.write("User logged in\n")
-
-    with open("log.txt", "r") as file:
-        history = file.read()
-        print("Full Log History:")
-        print(history)
-
-log_user_activity()
-
-# question 4
-def get_grades(student_grades):
-    x = input("Your name: ")
-
-    try:
-        print(f"Your grade is {student_grades[x]}")
-        return student_grades[x]
-    except KeyError:
-        return "Student not found in the system"
-
-grades = {
-    "mathew": 55,
-    "kudus": 77,
-    "ares": 98,
-}
-
-print(get_grades(grades))
+#question 4
+# class rectangle:
+#     def __init__(self,width,height):
+#         self.height=height
+#         self.width=width
+        
+#     def area(self):
+#         return self.height * self.width
+# values=rectangle(4,5)
+# print(values.area())
 
 # question 5
-def sales_number(values):
-    i = 0
-    with open("values.txt", "w") as file:
-        while i < len(values):
-            file.write(f"{values[i]}\n")
-            i += 1
-
-    with open("values.txt", "r") as file:
-        content = file.read()
-        print(content)
-
-
-values = [200, 450, 300, 700]
-sales_number(values)
+# class student:
+#     def __init__(self):
+#         self.__grade=None
+    
+#     def set_grade(self,grade):
+#         self.__grade=grade
+    
+#     def get_grade(self):
+#         return self.__grade
+# values=student()
+# values.set_grade("A")
+# print(values.get_grade())
 
 # question 6
-def calculator():
-    sales = []
+# class employee:
+#     def __init__(self,name,salary):
+#         self.name=name
+#         self.salary=salary
+        
+#     def annual_salary(self):
+#         return f"{self.name}'s annual salary is {self.salary * 12}"
+# data=employee("nati",100000)
+# print(data.annual_salary())
 
-    try:
-        with open("values.txt", "r") as file:
-            lines = file.readlines()
-
-        for line in lines:
-            line = line.strip()
-
-            try:
-                number = int(line)     
-                sales.append(number)
-            except ValueError:
-                continue
-
-        total = sum(sales)
-        print("Total sales:", total)
-
-    except FileNotFoundError:
-        print("values.txt not found.")
-
-
-calculator()
-
-# question 7
-def fizzbuzz(n):
-    answer=[]
-    for i in range(1,n+1):
-        if i % 3 == 0 and i % 5 ==0:
-            answer.append("fizzbuzz")
-        elif i % 3 == 0:
-            answer.append("fizz")
-        elif i % 5 ==0:
-            answer.append("buzz")
-        else:
-            answer.append(str(i))
-    return answer
-n=int(input("enter a numbeer "))
-print(fizzbuzz(n))
+#question 7
+# class library:
+#     def __init__(self):
+#         self.book=None
+    
+#     def add_books(self,book):
+#         self.book=book
+    
+#     def get_book(self):
+#         return self.book
+# data=library()
+# data.add_books("dertogada")
+# print(data.get_book())
 
 # question 8
-def moveZeroes(nums):
-    i = 0
-    
-    for num in nums:
-        if num != 0:
-            nums[i] = num
-            i += 1
-            
-    while i < len(nums):
-        nums[i] = 0
-        i += 1
-
-    return nums
-x=int(input("how many numbers you want to enter? "))
-nums=[]
-h=0
-while h < x:
-    m=int(input(f"enter your {h+1} number "))
-    nums.append(m)
-    h+=1
-print(moveZeroes(nums))
+# class Animal:
+#     def make_sound(self):
+#         print("animal make sound")
+        
+# class Cat(Animal):
+#     def make_sound(self):
+#         print("cat says meaw")
+# animal=Animal()
+# cat=Cat()
+# animal.make_sound()
+# cat.make_sound()
 
 # question 9
-def twoSum(nums, target):
-    seen = {} 
-
-    for i, num in enumerate(nums):
-        diff = target - num
-
-        if diff in seen:
-            return [seen[diff], i]
-
-        seen[num] = i
-x=int(input("how many numbers you want to  enter? "))
-nums=[]
-h=0
-while h < x:
-    m=int(input(f"enter your {h+1} number "))
-    nums.append(m)
-    h+=1
-target=int(input("enter your target number "))
-print(twoSum(nums, target))
+# class Vehicle:
+#     def __init__(self,brand,year):
+#         self.brand=brand
+#         self.year=year
+#     def info(self):
+#         return f"brand {self.brand}\n year {self.year}"
+        
+# class Car(Vehicle):
+#     def __init__(self,brand,year,model):
+#         super().__init__(brand,year)
+#         self.model=model
+        
+#     def info(self):
+#         return f"brand {self.brand}\n year {self.year}\n model {self.model}\n"
+# vehicle=Vehicle("toyota",1998)
+# car=Car("toyota",1998,"hilux")
+# print(vehicle.info())
+# print(car.info())
 
 # question 10
-def Palindrome(x):
-    if x < 0:
-        return False
-    
-    return str(x) == str(x)[::-1]
-x=int(input("enter number"))
-print(Palindrome(x))
-
-# question 11
-def mySqrt(x):
-    
-    if x < 2:  
-        return x
-
-    left, right = 1, x // 2
-    
-
-    while left <= right:
-        mid = (left + right) // 2
-        if mid * mid == x:
-            return mid
-        elif mid * mid < x:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return right
-x=int(input(" enter non negative number "))
-print(mySqrt(x))
-
-#question 12
-def calculator(x):
-    value=[]
-    i=0
-    with open("numbers.txt","a") as file:
-        while i < len(x):
-            file.write(f"{x[i]}\n")
-            i+=1
-    with open("numbers.txt","r") as file:
-        lines=file.readlines()
-    for line in lines:
-        line = line.strip()
+class Bank_account:
+    def __init__(self,balance=0):
+        self.__balance=balance
         
-        try:
-            number=int(line)
-            value.append(number)
-        except ValueError:
-            continue
-    total=sum(value)
-    return total
-x=[]
-h=int(input("number of values : "))
-i=0
-while i < h:
-    j=input(f" enter value {i+1} : ")
-    x.append(j)
-    i+=1
-print(calculator(x))
+    def Deposit(self,amount):
+        self.__balance+=amount
+    
+    def Withdrawal(self,amount):
+        if amount>self.__balance:
+            return "insufficient balance"
+        self.__balance-=amount
+    
+    def Get_balance(self):
+        return self.__balance
+        
 
-#question 13
-def converter(word):
-    upper=[]
-    i=0
-    with open ("words.txt","w") as file:
-        while i < len(word):
-            file.write(f"{word[i]}\n")
-            i+=1
-    with open ("words.txt","r") as file:
-        lines=file.readlines()
-
-    for line in lines:
-        line=line.strip()
-
-        try:
-            if line.isdigit():
-                continue
-            temp = line.upper()
-            upper.append(temp)
-        except ValueError:
-            continue
-    return upper
-word=["abs",765,"honey","SDCG"]
-
-print(converter(word))
-
-# question 14
-def get_scores(student_grades):
-    x = input("Your name: ").lower()
-
-    try:
-        return f"your grade is : {student_grades[x]}"
-    except KeyError:
-        return "Student not found in the system"
-
-score = {
-    "john": 85,
-    "sara": 92,
-    "fraol": 78,
-}
-
-print(get_scores(score)) 
-
-#question 15
-import re
-
-def word_frequencies(sentence):
-    sentence = sentence.lower()                     
-    words = re.findall(r"\b\w+\b", sentence)        
-    freq = {}                                     
-    for w in words:                                 
-        freq[w] = freq.get(w, 0) + 1                
-    return freq                                     
-
-s = input("Enter a sentence: ")                    
-result = word_frequencies(s)                       
-print(result)                                      
-
-# question 16
-def exchanger(name_key):
-    dict = {}
-
-    for name, grade in name_key.items():
-        try:
-            dict[grade].append(name)
-        except KeyError:
-            dict[grade] = [name]
-
-    return dict
-
-grades = {"John": "A", "Sara": "B", "Musa": "A"}
-print(exchanger(grades))
+account=Bank_account()
+account.Deposit(50)
+account.Withdrawal(70)
+print(account.Get_balance())
